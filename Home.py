6f98f7abe231ca819fac74e708d0a83f893fa670
AdrienDBe <95971759+AdrienDBe@users.noreply.github.com>
2022-06-19 21:04:18 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -110,7 +110,7 @@ with st.container():
     def local_css(file_name):
         with open(file_name) as f:
             st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-    local_css("style.css")
+    local_css("style/style.css")
 
     contact_form_container = st.container()
     with contact_form_container:
