8e6d7300afeaf527bff036ece12fccf1c6d36a74
AdrienDBe <95971759+AdrienDBe@users.noreply.github.com>
2022-06-19 22:18:28 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -107,7 +107,6 @@ with skills_container:
 
 # ---- CONTACT FORM ----
 # Use local CSS
-@cache
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
