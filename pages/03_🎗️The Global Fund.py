40be463cd21f06462d83155114bc17011225f27c
AdrienDBe <95971759+AdrienDBe@users.noreply.github.com>
2022-07-30 20:15:07 +0200

    Update 03_🎗️The Global Fund.py

--- pages/03_🎗️The Global Fund.py
+++ pages/03_🎗️The Global Fund.py
@@ -660,6 +660,11 @@ if dataset == "Disbursement records":
 
 if dataset == "Grant Agreements":
 
+    st.title("Global Fund API explorer")
+    col1, col2 = st.columns([20,50],gap='medium')
+    dataset = col1.radio("",('Disbursement records', 'Grant Agreements', 'Implementation periods'))
+    col2.markdown("<br>![Alt Text](https://media.giphy.com/media/shNla43zRRWazpOS2X/giphy.gif)", unsafe_allow_html=True)     
+
     # Loading GF API
     count = 0
     gif_runner = st.empty()
