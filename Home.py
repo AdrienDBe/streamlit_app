4b53e76d8a6a29aa82653d3b261b8854c3a003ef
AdrienDBe <95971759+AdrienDBe@users.noreply.github.com>
2023-01-15 20:00:49 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -15,8 +15,6 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.set_page_config(page_title="Adrien Debruge", layout="wide")
-
 # Custom function for printing text
 def txt3(a, b):
   col1, col2 = st.columns([1,2])
