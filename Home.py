312210766cf2e910e6f34bcc7c1a9bf8bcbb996a
AdrienDBe <95971759+AdrienDBe@users.noreply.github.com>
2023-01-15 14:48:37 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -57,7 +57,7 @@ with work_container:
         st.markdown('''
         **INFORMATION & PROCESS MANAGEMENT** `Operational efficiency within the Grant Management Division`
         \n- Business Process Monitoring & Analytics
-        \Develop and issuance of milestone-driven business process reporting and monitoring for distribution to and use by the Grant Management Division directorate
+        \nDevelop and issuance of milestone-driven business process reporting and monitoring for distribution to and use by the Grant Management Division directorate
         \n- Business Process Management<brEnsure resolution of challenges escalated by Country Teams, provide analytical support to Grant Operations, Finance and Risk departments
         \n- Business Process Enhancement<brDefine enhancements and develop business requirements, review the translation into functional specifications, for the grant lifecycle processes, procedures, system solutions, data flows, quality & reporting
         ''')
