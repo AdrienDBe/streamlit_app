8ffc1726daa425656f6a2e2a95a50042a91c500b
AdrienDBe <95971759+AdrienDBe@users.noreply.github.com>
2023-01-15 14:40:06 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -56,9 +56,9 @@ with work_container:
     with st.expander("2022 – today | The Global Fund : Business Process Reporting Officer"):
         st.markdown('''
         **INFORMATION & PROCESS MANAGEMENT** `Operational efficiency within the Grant Management Division`
-        \n- Business Process Monitoring & Analytics: Develop and issuance of milestone-driven business process reporting and monitoring for distribution to and use by the Grant Management Division directorate
-        \n- Business Process Management: Ensure resolution of challenges escalated by Country Teams, provide analytical support to Grant Operations, Finance and Risk departments
-        \n- Business Process Enhancement: Define enhancements and develop business requirements, review the translation into functional specifications, for the grant lifecycle processes, procedures, system solutions, data flows, quality & reporting
+        \n- Business Process Monitoring & Analytics<br>Develop and issuance of milestone-driven business process reporting and monitoring for distribution to and use by the Grant Management Division directorate
+        \n- Business Process Management<brEnsure resolution of challenges escalated by Country Teams, provide analytical support to Grant Operations, Finance and Risk departments
+        \n- Business Process Enhancement<brDefine enhancements and develop business requirements, review the translation into functional specifications, for the grant lifecycle processes, procedures, system solutions, data flows, quality & reporting
         ''')
     with st.expander("2020 – 2022 | Cepheid : Lead AccessCare Program Manager"):
         st.markdown('''
