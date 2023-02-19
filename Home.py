6449e10f22d84f9448130546b97cd18f91877839
AdrienDBe <95971759+AdrienDBe@users.noreply.github.com>
2023-02-19 16:54:15 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -49,6 +49,21 @@ with st.container():
         st.image(img,width = 300)
 
 # ---- Work experience ----
+path = "Images/Lottie_work.json"
+with open(path, "r") as file:
+    url = json.load(file)
+
+st.title("Adding Lottie Animation in Streamlit WebApp")
+
+st_lottie(url,
+          reverse=True,
+          height=400,
+          width=400,
+          speed=1,
+          loop=True,
+          quality='high',
+          key='Car'
+          )
 work_container = st.container()
 with work_container:
     st.write("---")
