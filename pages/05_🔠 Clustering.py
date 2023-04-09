55bb431a7cba233c4267ace269ab9f89f5969a09
AdrienDBe <95971759+AdrienDBe@users.noreply.github.com>
2023-04-09 12:53:47 +0200

    Update 05_🔠 Clustering.py

--- pages/05_🔠 Clustering.py
+++ pages/05_🔠 Clustering.py
@@ -18,6 +18,7 @@ st.title("Clustering tool")
 
 # Define function to load data
 def load_data(file):
+    # Load data from file
     data = pd.read_csv(file)
     # Create a boolean mask that is True for rows with at least one NaN value
     has_empty_mask = data.isna().any(axis=1)
@@ -27,7 +28,8 @@ def load_data(file):
     data.dropna(inplace=True)
     data.reset_index(drop=True,inplace=True)
     categorical_cols = list(data.select_dtypes(include=['object','category']))
-    return data, categorical_cols, data_HasEmpty,lenght_data_HasEmpty
+    num_cols = list(data.select_dtypes(include=['int64', 'float64']))
+    return data, categorical_cols, num_cols, data_HasEmpty,lenght_data_HasEmpty
 
 def has_categorical_data(data):
     categorical_cols = data.select_dtypes(include=['object', 'category']).columns
@@ -48,23 +50,40 @@ def run_clustering(data, num_clusters):
     clusters = kmeans.predict(scaled_data)
     return clusters
 
+# Add radio button to choose between uploading a file and using a sample file
+use_sample = st.radio('Use a sample dataset?', ['No', 'Yes'],horizontal=True)
+
 # Load data
 col1, col2 = st.columns([2, 1], gap='small')
-data_file = col1.file_uploader('Upload your dataset', type=['csv', 'xlsx'],label_visibility="collapsed")
-if data_file is None:
-    info = col2.info('Upload your data to start clustering')
-if data_file is not None:
-    data, categorical_cols, data_HasEmpty,lenght_data_HasEmpty = load_data(data_file)
-    # Show data summary
-    if len(data_HasEmpty) >0:
-        col2.warning('{} missing values have been removed from your dataset, {} remaining rows'.format(len(data_HasEmpty),len(data)))
+if use_sample =="No":
+    data_file = col1.file_uploader('Upload your dataset', type=['csv'],label_visibility="collapsed")
 
+if use_sample == 'No' and data_file is None:
+    info = col2.info('Upload your data to start clustering')
+
+if use_sample == 'Yes':
+    # Load sample data
+    data_file = "C:/Users/adrie/Downloads/Customers.csv"
+
+
+if data_file is not None:
+    # Load data from file
+    data, categorical_cols, num_cols, data_HasEmpty,lenght_data_HasEmpty = load_data(data_file)
     tab1, tab2 = st.tabs(["Cleaned dataset", "Data type summary"])
     with tab1:
         st.dataframe(data,height= 300)
     with tab2:
+        col1, col2 = st.columns([1, 1], gap='small')
         # Show column names and types
-        st.dataframe(data.dtypes)
+        col1.dataframe(data.dtypes)
+        # Show data summary
+        if len(data_HasEmpty) > 0:
+            if use_sample == "No":
+                col2.warning('{} missing values have been removed from your dataset, {} remaining rows'.format(
+                    len(data_HasEmpty), len(data)))
+            if use_sample == "Yes":
+                col2.warning('{} missing values have been removed from the sample dataset, {} remaining rows'.format(
+                    len(data_HasEmpty), len(data)))
 
     col1, col2 = st.columns([1, 1], gap='small')
     # Allow user to choose which columns to include in clustering analysis
@@ -75,8 +94,9 @@ if data_file is not None:
     # Create second dataset with selected columns only
     selected_data = data[selected_columns]
 
-    # Check if any categorical columns are in selected_columns
+    # Check if any categorical and numerical columns are in selected_columns
     categorical_cols_selected = [col for col in categorical_cols if col in selected_columns]
+    numerical_cols_selected = [col for col in num_cols if col in selected_columns]
 
     # If there are any categorical columns in selected_columns, Use OneHotEncoder to one-hot encode them
     if len(categorical_cols_selected) > 0:
@@ -93,15 +113,15 @@ if data_file is not None:
         data_clean = data[selected_columns].copy()
 
     if len(selected_columns) == 0:
-        col2.info('Select at least one column for clustering analysis.')
+        col1.info('Select at least one column for clustering analysis.')
     if len(selected_columns) != 0 and len(categorical_cols_selected) == selected_data.shape[1]:
-        col2.info('Select at least one numerical value')
-
-    col2.caption("Optimal cluster number: Elbow Method")
-
-    tab1, tab2 = st.tabs(["Visualize clusters", "Display dataframe / download results"])
+        col1.info('Select at least one numerical column too')
+    if len(selected_columns) >= 2:
+        col2.caption("Optimal cluster number: Elbow Method")
 
     if len(selected_columns) != 0 and len(categorical_cols_selected) != selected_data.shape[1]:
+
+        tab1, tab2 = st.tabs(["Visualize clusters", "Display dataframe / download results"])
 
         # Run clustering algorithm
         num_clusters = col1.slider('Number of clusters', value = 3, min_value=2, max_value=10)
@@ -150,17 +170,60 @@ if data_file is not None:
             # If only 1 column, use strip plot with hue as clusters
             if num_cols == 1 and len(categorical_cols_selected) ==0 :
                 fig = px.strip(selected_data, x=selected_data.columns[0], color=selected_data.columns[1], stripmode = "overlay")
+                fig.update_layout(
+                    title=None,
+                    margin=dict(t=0, b=10), # set the bottom margin to 100
+                    height=600,  # set the plot height to 500 pixels
+                    font=dict(size=18),
+                    title_font=dict(size=24),
+                    legend_font=dict(size=16),
+                    xaxis_title_font=dict(size=20),
+                    yaxis_title_font=dict(size=20),
+                    xaxis_tickfont=dict(size=16),
+                    yaxis_tickfont=dict(size=16))
+                st.plotly_chart(fig)
+
+            # If 1 categorical and 1 numerical columns, use bar plot:
+            if len(numerical_cols_selected) ==1 and len(categorical_cols_selected) ==1:
+                fig = px.bar(selected_data, x=selected_data.columns[0], y=selected_data.columns[1])
+                fig.update_layout(
+                    title=None,
+                    margin=dict(t=0, b=10), # set the bottom margin to 100
+                    height=600,  # set the plot height to 500 pixels
+                    font=dict(size=18),
+                    title_font=dict(size=24),
+                    legend_font=dict(size=16),
+                    xaxis_title_font=dict(size=20),
+                    yaxis_title_font=dict(size=20),
+                    xaxis_tickfont=dict(size=16),
+                    yaxis_tickfont=dict(size=16))
                 st.plotly_chart(fig)
 
             # If 2 columns without categorical values, use scatter plot with color as clusters
             elif num_cols == 2 and len(categorical_cols_selected) ==0 :
-                fig = px.scatter(data_clean, x=selected_data.columns[0], y=selected_data.columns[1], color=selected_data["Cluster"])
+                fig = px.scatter(data_clean, x=selected_data.columns[0], y=selected_data.columns[1], color=selected_data["Cluster"], labels={'color': 'Clusters'})
+                fig.update_layout(
+                    margin=dict(t=0, b=10), # set the bottom margin to 100
+                    height=600,  # set the plot height to 500 pixels
+                    font=dict(size=18),
+                    title_font=dict(size=24),
+                    legend_font=dict(size=16),
+                    xaxis_title_font=dict(size=20),
+                    yaxis_title_font=dict(size=20),
+                    xaxis_tickfont=dict(size=16),
+                    yaxis_tickfont=dict(size=16))
+
                 st.plotly_chart(fig)
 
             # If 3 columns without categorical values, use 3D scatter plot with color as clusters
             elif num_cols == 3 and len(categorical_cols_selected) ==0 :
                 fig = px.scatter_3d(data_clean, x=selected_data.columns[0], y=selected_data.columns[1], z=selected_data.columns[2],
-                                    color=selected_data["Cluster"])
+                                    color=selected_data["Cluster"], labels={'color': 'Clusters'})
+                fig.update_traces(mode='markers',
+                                  marker=dict(size=7,
+                                              line=dict(width=2,
+                                                        color='black'))
+                                  )
 
                 camera = dict(
                     eye=dict(x=1.5, y=1.7, z=0.8),
@@ -171,8 +234,10 @@ if data_file is not None:
                     title=None,
                     margin=dict(t=0, b=10), # set the bottom margin to 100
                     height=600,  # set the plot height to 500 pixels
-                    scene_camera = camera
+                    scene_camera = camera,
+                    font=dict(size=14)
                 )
+
 
                 st.plotly_chart(fig)
 
@@ -184,13 +249,26 @@ if data_file is not None:
             elif num_cols == 1 and len(categorical_cols_selected) == 1 :
                 fig = px.scatter(selected_data, x=selected_data.columns[0], y=selected_data.columns[1],
                                     color=selected_data["Cluster"])
+                fig.update_layout(
+                    margin=dict(t=0, b=10), # set the bottom margin to 100
+                    height=600,  # set the plot height to 500 pixels
+                    font=dict(size=18),
+                    title_font=dict(size=24),
+                    legend_font=dict(size=16),
+                    xaxis_title_font=dict(size=20),
+                    yaxis_title_font=dict(size=20),
+                    xaxis_tickfont=dict(size=16),
+                    yaxis_tickfont=dict(size=16))
                 st.plotly_chart(fig)
 
             # If more than 3 columns, use PCA to reduce dimensions to 2 and plot scatter plot with color as clusters
-            else:
+            elif num_cols > 3:
+                # Scale the data
+                scaler = StandardScaler()
+                scaled_data = scaler.fit_transform(data_clean)
                 # Fit PCA model to data_clean
                 pca = PCA(n_components=2)
-                data_pca = pca.fit_transform(data_clean)
+                data_pca = pca.fit_transform(scaled_data)
 
                 # Create dataframe with PCA results
                 pca_df = pd.DataFrame(data_pca, columns=['PC1', 'PC2'])
@@ -204,34 +282,33 @@ if data_file is not None:
                     pca_df['Cluster'] = pca_df['Cluster'].apply(lambda x: 'Cluster ' + str(x))
 
                     # Plot scatter plot with color as clusters
-                    fig = px.scatter(pca_df, x='PC1', y='PC2', color='Cluster')
+                    fig = px.scatter(pca_df, x='PC1', y='PC2', color='Cluster', labels={'color': 'Clusters'})
                     fig.update_layout(
-                        autosize=False,
-                        margin=dict(
-                            l=0,
-                            r=0,
-                            b=0,
-                            t=50,
-                            pad=4,
-                            autoexpand=True),
-                        height=600,
-                        paper_bgcolor='rgba(0,0,0,0)',
-                        plot_bgcolor='rgba(0,0,0,0)',
-                        legend_title='Component')
-                    fig.update_traces(opacity=1, marker=dict(line=dict(width=0)))
+                        margin=dict(t=0, b=10),  # set the bottom margin to 100
+                        height=600,  # set the plot height to 500 pixels
+                        font=dict(size=18),
+                        title_font=dict(size=24),
+                        legend_font=dict(size=16),
+                        xaxis_title_font=dict(size=20),
+                        yaxis_title_font=dict(size=20),
+                        xaxis_tickfont=dict(size=16),
+                        yaxis_tickfont=dict(size=16))
                     fig.update_xaxes(showgrid=False, zeroline=True, title_text="")
                     st.plotly_chart(fig, use_container_width=True)
 
-    with tab2:
-        # Show clustering results
-        st.write('Clustering results:')
-        st.dataframe(selected_data)
-
-        # Allow user to download clustering results
-        #st.download_button('Download clustering results', data=pd.DataFrame({'Cluster': selected_data}).to_csv(index=False), file_name='clustering_results.csv')
-
-else:
-    st.write('Please upload a CSV or Excel file.')
-
-
-
+        with tab2:
+            # Show clustering results
+            st.write('Clustering results:')
+            col1, col2 = st.columns([1, 1], gap='small')
+            col1.dataframe(selected_data)
+            # Allow user to download clustering results
+            @st.cache_data
+            def convert_df(dl_file):
+                # IMPORTANT: Cache the conversion to prevent computation on every rerun
+                return dl_file.to_csv().encode('utf-8')
+            csv = convert_df(selected_data)
+            col2.download_button(
+                label="Download data as CSV",
+                data=csv,
+                file_name='Clustered_dataset.csv'
+            )
