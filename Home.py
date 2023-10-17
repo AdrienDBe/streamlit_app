9aa49a9dbbe7e2b4f6e78813f9ee37aa5e9b9b5c
AdrienDBe <95971759+AdrienDBe@users.noreply.github.com>
2023-10-17 08:06:42 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -75,9 +75,9 @@ with work_container:
     </style>
     """, unsafe_allow_html=True)
 
-    with col2.expander("2022 – Today | The Global Fund : Officer, Operational Efficiency"):
+    with col2.expander("2022 – Today | The Global Fund : Officer, Grant Management Division"):
         st.markdown('''
-        **GRANT MANAGEMENT DIVISION**
+        **OPERATIONAL EFFICIENCY**
         \n- Business Process Monitoring & Analytics
         \nDevelop and issuance of milestone-driven business process reporting and monitoring for distribution to and use by the Grant Management Division directorate
         \n- Business Process Management
