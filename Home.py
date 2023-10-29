fe52eabbe5de53745c548c1261c76060fb868bd2
AdrienDBe <95971759+AdrienDBe@users.noreply.github.com>
2023-10-29 11:56:18 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,6 +19,11 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 local_css("style/style.css")
+
+# Use CSS for background
+with open('./style/wave.css') as f:
+    css = f.read()
+st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
     
 # ---- HEADER SECTION ----
 with st.container():
