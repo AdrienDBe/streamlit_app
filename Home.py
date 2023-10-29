a17ab63eb30418f85e0c022d34e5af5b4a297de1
AdrienDBe <95971759+AdrienDBe@users.noreply.github.com>
2023-10-29 12:40:16 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -28,7 +28,7 @@ st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 # ---- HEADER SECTION ----
 with st.container():
     st.title("Adrien Debruge")
-    col1, col2 = st.columns((10,1,2,2))
+    col1, col2, col3, col4  = st.columns((10,1,2,2))
     with col1:
         st.subheader("Welcome:wave:")
         st.markdown("<p style='text-align: justify; font-size: 18px;'>"
