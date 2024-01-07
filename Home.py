2dc515bd4aad28207fb930d7ebd3b02ce70aecc0
AdrienDBe <95971759+AdrienDBe@users.noreply.github.com>
2024-01-07 19:17:46 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -41,7 +41,8 @@ with st.sidebar:
     st.markdown("<a style='text-decoration: none; color: white;' href='https://adrien-clustering.streamlit.app/'>🔠 Clustering Overview</a>", unsafe_allow_html=True)
     st.markdown("<a style='text-decoration: none; color: white;' href='https://clustering-tool.streamlit.app/'>🧰 Clustering Tool</a>", unsafe_allow_html=True)
     st.text('API Explorer')
-    st.markdown("<a style='text-decoration: none; color: white;' href='https://theglobalfund-api.streamlit.app'>🎗️ The Global Fund API</a>", unsafe_allow_html=True)
+    st.markdown("<a style='text-decoration: none; color: white;' href='https://theglobalfund-api.streamlit.app'>🎗️ The Global Fund API: Grants implementations</a>", unsafe_allow_html=True)
+    st.markdown("<a style='text-decoration: none; color: white;' href='https://theglobalfund-disbursements.streamlit.app/.app'>🎗️ The Global Fund API: Grants Disbursements</a>", unsafe_allow_html=True)
     st.markdown("<a style='text-decoration: none; color: white;' href='https://world-health-organization-api.streamlit.app/'>⚕️ WHO Indicators</a>", unsafe_allow_html=True)
 
     
